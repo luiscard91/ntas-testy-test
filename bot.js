c59a91655d0d5b12767f59cd18989acba958236c
luiscard91 <Luiscard91@gmail.com>
2017-09-09 13:16:23 -0400

    "looks like shit" auto img response 1st attempt

--- bot.js
+++ bot.js
@@ -5,8 +5,9 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botCoolRegex = /^\/cool guy$/;
-      botHueRegex = /^\/hue$/;
+      botCoolRegex = /^\/cool guy$/;// respond with cool acii
+      botHueRegex = /^\/hue$/;  //respnod with cool hue gif
+      botImgRegex = /((.)*\.jpg$)|((.)*\.png$)/;  //respond to images
 
   if(request.text && botCoolRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -16,11 +17,50 @@ function respond() {
     this.res.writeHead(200);
     postHueMessage();
     this.res.end();
+  } else if(request.text && botImgRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postShitMessage();
+    this.res.end();
   } else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
+}
+
+function postShitMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = 'Looks like shit';
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
 }
 
 function postHueMessage() {
