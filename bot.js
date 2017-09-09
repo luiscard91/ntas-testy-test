f1c8bcf57ee88b154f83e18d80b49d0bdc92af57
luiscard91 <Luiscard91@gmail.com>
2017-09-09 00:58:48 -0400

    bot changes 1st attempt

--- bot.js
+++ bot.js
@@ -5,17 +5,65 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
 
-  if(request.text && botRegex.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
+      botRegex = [/^\/cool guy$/, /^\/hue/];
+      //botRegex = /^\/cool guy$/;
+
+
+  for (var i = 0; i <= botRex.length - 1; i++) {
+    if(request.text && botRegex[i].test(request.text)) {
+      if(i == 0) {
+        this.res.writeHead(200);
+        postMessage();
+        this.res.end();        
+      } else if(i == 1) {
+        this.res.writeHead(200);
+        postHueGif();
+        this.res.end(); 
+      }
+    } else {
+      console.log("don't care");
+      this.res.writeHead(200);
+      this.res.end();
+    }
+    
   }
+
+}
+
+function postHueGif() {
+  var botResponse, options, body, botReq;
+
+  botResponse = 'https://puu.sh/xv87K/a0135e2de6.gif';
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
 }
 
 function postMessage() {
